b55c3ad14008e66acc2ed5db679766763ad5b527
Ahmad Arif Faizin <faizin.arief@gmail.com>
2022-09-27 13:25:21 +0700

    Update login-script.js

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -5,10 +5,30 @@
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
+/** @module Login-Script */
+/** 
+* Membuat variabel loginFormElement untuk tampilan form.
+* @constant {HTMLElement}
+*/
 const loginFormElement = document.querySelector('#loginForm');
+
+/** 
+* Membuat variabel inputEmailElement untuk tampilan input email.
+* @constant {HTMLElement}
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
+
+ 
+/** 
+* Membuat variabel inputPasswordElement untuk tampilan input password.
+* @constant {HTMLElement}
+*/
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/** 
+ * Membuat variabel expectedEmail untuk menyimpan informasi email sementara.
+ * @constant {string}
+ */
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
